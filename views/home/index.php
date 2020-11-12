2940f766bba02b85350febf422721af50d1ba63b
Khải Nguyễn Ngọc <17520607@ms.uit.edu.vn>
2020-11-12 09:37:37 +0700

    commit

--- views/home/index.php
+++ views/home/index.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+echo phpinfo();
+// $serverName = "serverName\\sqlexpress"; //serverName\instanceName
+// $connectionInfo = array( "Database"=>"dbName", "UID"=>"userName", "PWD"=>"password");
+// $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+// if( $conn ) {
+//      echo "Connection established.<br />";
+// }else{
+//      echo "Connection could not be established.<br />";
+//      die( print_r( sqlsrv_errors(), true));
+// }
+?>
+
     Home > Index
 </body>
 </html>
